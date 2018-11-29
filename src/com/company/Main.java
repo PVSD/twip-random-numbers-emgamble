713ffe20bef6f9253430ebf0599c5d375ede2bbb
eg913 <eg913@pvsd.org>
2018-11-29 11:21:58 -0500

    not done obvs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        boolean value [] = new boolean[100];
+
+
+        for(int i = 0; i <= 100; i++){
+
+
+            int rdm = (int) Math.random()* 100;
+            value[rdm]= true;
+
+
+        }
+
+
+        
+
+
+
+
     }
 }
