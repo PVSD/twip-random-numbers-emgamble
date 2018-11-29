77e4fb46a5dea278066265e0ea88d7000a05243a
eg913 <eg913@pvsd.org>
2018-11-29 14:57:30 -0500

    not done obvs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,43 @@
 package com.company;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
 
 
         boolean value [] = new boolean[100];
+        Random rand = new Random();
+        boolean yes;
+        int count = 0;
 
 
         for(int i = 0; i <= 100; i++){
 
 
-            int rdm = (int) Math.random()* 100;
-            value[rdm]= true;
+            while(yes == false){
+                int rdm = rand.nextInt(100);
+                value[rdm]= true;
+                count++;
 
+                for (boolean x: value) {
+                    if(x==false) {
+                        yes = false;
+                    }
+                    else
+                        yes = true;
+
+
+                }
+            }
 
         }
+        int average = count/100;
+        System.out.println(average);
+        System.out.println(count);
 
 
-        
+
+
 
 
 
